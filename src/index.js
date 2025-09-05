da3712f5ec5c417e66f857de8fc6b12a8d6b68a1
Jaysinghgautam <gjaysingh53@gmail.com>
2025-09-05 12:53:54 +0530

    added controler register

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import express from "express";
-const app = express();
+import { app } from "./app.js";
+// const app = express();
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 dotenv.config(); 
